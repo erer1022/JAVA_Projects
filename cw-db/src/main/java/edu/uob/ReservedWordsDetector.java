af14fbef43b16ff6a445b76189e2ca57443b6eb4
erer1022 <146210444+erer1022@users.noreply.github.com>
2024-03-21 14:12:36 -0500

    Update Reserved Words

--- cw-db/src/main/java/edu/uob/ReservedWordsDetector.java
+++ cw-db/src/main/java/edu/uob/ReservedWordsDetector.java
@@ -10,27 +10,47 @@ public class ReservedWordsDetector {
     public ReservedWordsDetector() {
         reservedWords = new HashSet<>();
         reservedWords.add("use");
+        reservedWords.add("USE");
         reservedWords.add("create");
+        reservedWords.add("CREATE");
         reservedWords.add("drop");
+        reservedWords.add("DROP");
         reservedWords.add("alter");
+        reservedWords.add("ALTER");
         reservedWords.add("insert");
+        reservedWords.add("INSERT");
         reservedWords.add("into");
+        reservedWords.add("INTO");
         reservedWords.add("select");
+        reservedWords.add("SELECT");
         reservedWords.add("update");
+        reservedWords.add("UPDATE");
         reservedWords.add("delete");
+        reservedWords.add("DELETE");
         reservedWords.add("join");
+        reservedWords.add("JOIN");
         reservedWords.add("database");
+        reservedWords.add("DATABASE");
         reservedWords.add("table");
+        reservedWords.add("TABLE");
         reservedWords.add("*");
         reservedWords.add("where");
+        reservedWords.add("WHERE");
         reservedWords.add("on");
+        reservedWords.add("ON");
         reservedWords.add("like");
+        reservedWords.add("LIKE");
         reservedWords.add("set");
+        reservedWords.add("SET");
         reservedWords.add("add");
+        reservedWords.add("ADD");
+        reservedWords.add("values");
+        reservedWords.add("VALUES");
+        reservedWords.add("from");
+        reservedWords.add("FROM");
     }
 
     public boolean isReservedWord(String word) {
         return reservedWords.contains(word);
     }
 }
-
