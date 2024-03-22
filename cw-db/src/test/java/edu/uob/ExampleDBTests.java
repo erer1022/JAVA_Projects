b07aae7c33a05113819785587adbeccc6901620e
erer1022 <146210444+erer1022@users.noreply.github.com>
2024-03-21 23:31:29 -0500

    Update ExampleDBTests.java

--- cw-db/src/test/java/edu/uob/ExampleDBTests.java
+++ cw-db/src/test/java/edu/uob/ExampleDBTests.java
@@ -28,7 +28,7 @@ public class ExampleDBTests {
     private String sendCommandToServer(String command) {
         // Try to send a command to the server - this call will timeout if it takes too long (in case the server enters an infinite loop)
         return assertTimeoutPreemptively(Duration.ofMillis(1000), () -> { return server.handleCommand(command);},
-        "Server took too long to respond (probably stuck in an infinite loop)");
+                "Server took too long to respond (probably stuck in an infinite loop)");
     }
 
     // A basic test that creates a database, creates a table, inserts some test data, then queries it.
@@ -99,6 +99,7 @@ public class ExampleDBTests {
         sendCommandToServer("CREATE TABLE marks (name, mark, pass);");
         sendCommandToServer("INSERT INTO marks VALUES ('Simon', 65, TRUE);");
         String response = sendCommandToServer("SELECT * FROM libraryfines;");
+        System.out.println(response);
         assertTrue(response.contains("[ERROR]"), "An attempt was made to access a non-existent table, however an [ERROR] tag was not returned");
         assertFalse(response.contains("[OK]"), "An attempt was made to access a non-existent table, however an [OK] tag was returned");
     }
@@ -113,7 +114,7 @@ public class ExampleDBTests {
         sendCommandToServer("INSERT INTO marks VALUES ('Rob', 35, FALSE);");
         sendCommandToServer("INSERT INTO marks VALUES ('Chris', 20, FALSE);");
 
-        String response3 = sendCommandToServer("DELETE FROM marks WHERE name == 'Sion';");
+        sendCommandToServer("DELETE FROM marks WHERE name == 'Sion';");
 
         String response2 = sendCommandToServer("SELECT * FROM marks;");
         System.out.println(response2);
@@ -132,6 +133,13 @@ public class ExampleDBTests {
         System.out.println(response2);
         String response3 = sendCommandToServer("DROP DATABASE database1;");
         System.out.println(response3);
+
+        String response4 = sendCommandToServer("DROP DATABASE database1;");
+        System.out.println(response4);
+
+        String response5 = sendCommandToServer("DROP TABLE data1;");
+        System.out.println(response5);
+
     }
 
     @Test
@@ -141,37 +149,59 @@ public class ExampleDBTests {
         sendCommandToServer("CREATE TABLE marks (name, mark, pass);");
         sendCommandToServer("INSERT INTO marks VALUES ('Simon', 65, TRUE);");
         sendCommandToServer("INSERT INTO marks VALUES ('Sion', 55, TRUE);");
-        String response = sendCommandToServer("UPDATE marks SET mark = 35, pass = FALSE WHERE name == 'Simon';");
-        System.out.println(response);
-
+        //multiple set clause, case insensitive
+        String response3 = sendCommandToServer("update marks set mark = 35, pass = FALSE where name == 'Simon';");
+        System.out.println(response3);
         String response1 = sendCommandToServer("SELECT * FROM marks;");
         System.out.println(response1);
+
+        //Wrong table
+        String response2 = sendCommandToServer("UPDATE test SET mark = 35, pass = FALSE WHERE name == 'Simon';");
+        System.out.println(response2);
+
+        //Missing setclause
+        String response4 = sendCommandToServer("UPDATE marks mark = 35 WHERE name == 'Simon';");
+        System.out.println(response4);
+        String response5 = sendCommandToServer("UPDATE marks set WHERE name == 'Simon';");
+        System.out.println(response5);
+
+        //Missing where clause
+        String response6 = sendCommandToServer("UPDATE marks SET mark = 35, pass = FALSE;");
+        System.out.println(response6);
+
+        //changing (updating) the ID of a record
+        String response8 = sendCommandToServer("UPDATE marks SET id = 2 WHERE name == 'Simon';");
+        System.out.println(response8);
     }
 
     @Test
     public void testALTER() {
         sendCommandToServer("CREATE DATABASE markbook;");
         sendCommandToServer("USE " + "markbook" + ";");
-        sendCommandToServer("CREATE TABLE marks (name, mark, pass);");
+        sendCommandToServer("CREATE TABLE marks (Name, Mark, Pass);");
         sendCommandToServer("INSERT INTO marks VALUES ('Simon', 65, TRUE);");
         sendCommandToServer("INSERT INTO marks VALUES ('Sion', 55, TRUE);");
         String response = sendCommandToServer("ALTER TABLE marks ADD age;");
         System.out.println(response);
-
         String response1 = sendCommandToServer("SELECT * FROM marks;");
         System.out.println(response1);
-
-        String response2 = sendCommandToServer("UPDATE marks SET age = 35 WHERE name == 'Simon';");
-        System.out.println(response2);
-
-        String response3 = sendCommandToServer("SELECT * FROM marks;");
-        System.out.println(response3);
-
+        /* treat column names as case-insensitive for querying, but preserve the case when storing them */
         String response4 = sendCommandToServer("ALTER TABLE marks DROP pass;");
         System.out.println(response4);
-
         String response5 = sendCommandToServer("SELECT * FROM marks;");
         System.out.println(response5);
+
+        /* attempting to remove the ID column from a table */
+        String response2 = sendCommandToServer("ALTER TABLE marks DROP id;");
+        System.out.println(response2);
+
+        /* attribute names containing . characters are not permitted by the BNF */
+        String response3 = sendCommandToServer("ALTER TABLE marks ADD hi.j;");
+        System.out.println(response3);
+
+        /* attribute names containing . characters are not permitted by the BNF */
+        String response6 = sendCommandToServer("ALTER TABLE marks ADD id;");
+        System.out.println(response6);
     }
 
     @Test
@@ -199,12 +229,19 @@ public class ExampleDBTests {
         System.out.println(response3);
         String response4 = sendCommandToServer("JOIN marks AND coursework ON id AND submission;");
         System.out.println(response4);
+        //Invalid command, missing "AND"
+        String response5 = sendCommandToServer("JOIN marks coursework ON id AND submission;");
+        System.out.println(response5);
+
+        //Check order
+        String response6 = sendCommandToServer("JOIN marks AND coursework ON submission AND id;");
+        System.out.println(response6);
     }
 
 
 
     @Test
-    public void whereclause(){
+    public void testSELECT(){
         String randomName = generateRandomName();
         sendCommandToServer("CREATE DATABASE " + randomName + ";");
         sendCommandToServer("USE " + randomName + ";");
@@ -215,7 +252,99 @@ public class ExampleDBTests {
         sendCommandToServer("INSERT INTO marks VALUES ('Chris', 20, FALSE);");
         String response = sendCommandToServer("SELECT * FROM marks WHERE (pass == TRUE) AND (mark >= 55);");
         System.out.println(response);
+
+        //Missing attribute
+        String response1 = sendCommandToServer("SELECT  FROM marks;");
+        System.out.println(response1);
+        //Wrong table name
+        String response2 = sendCommandToServer("SELECT * FROM test WHERE (pass == TRUE) AND (mark >= 55);");
+        System.out.println(response2);
+        //Wrong attribute name
+        String response4 = sendCommandToServer("select shidjs from MarKs where (pass == TRUE) and (mark >= 55);");
+        System.out.println(response4);
+        //multiple attributes
+        String response5 = sendCommandToServer("select name, mark from MarKs where (pass == TRUE) and (mark >= 55);");
+        System.out.println(response5);
+        //whitespace
+        String response6 = sendCommandToServer("select name, mark from MarKs where Name=='Chris';");
+        System.out.println(response6);
+
     }
 
+    @Test
+    public void testCREATE() {
+        String response = sendCommandToServer("create database " + "markbook" + ";");
+        System.out.println(response);
+        String response2 = sendCommandToServer("create database " + "MARKBOOK" + ";");
+        System.out.println(response2);
 
+        sendCommandToServer("use " + "markbook" + ";");
+        //case-insensitive
+        String response3 = sendCommandToServer("create table MARKS;");
+        System.out.println(response3);
+        String response4 = sendCommandToServer("CREATE TABLE marks;");
+        System.out.println(response4);
+        //Missing database name
+        String response1 = sendCommandToServer("CREATE DATABASE;");
+        System.out.println(response1);
+        //creating a table with duplicate column names
+        String response5 = sendCommandToServer("create table test (name, name, marks);");
+        System.out.println(response5);
+        //Invalid name
+        String response6 = sendCommandToServer("CREATE DATABASE hh.hh;");
+        System.out.println(response6);
+        /* treat column names as case-insensitive for querying,
+           but preserve the case when storing them */
+        String response7 = sendCommandToServer("create table column (Name, Gender, marks);");
+        System.out.println(response7);
+        String response8 = sendCommandToServer("select name from COLUMN;");
+        System.out.println(response8);
+    }
+
+    @Test
+    public void testINSERT() {
+        String randomName = generateRandomName();
+        sendCommandToServer("CREATE DATABASE " + randomName + ";");
+        sendCommandToServer("USE " + randomName + ";");
+        sendCommandToServer("CREATE TABLE marks (name, mark, pass);");
+        sendCommandToServer("insert into marks values ('Simon', 65, TRUE);");
+        sendCommandToServer("insert into MARKS values ('Sion', 55, TRUE);");
+        //Insert too much value
+        String response7 = sendCommandToServer("INSERT INTO marks VALUES ('Rob', 35, FALSE, xx);");
+        System.out.println("Insert too much value: " + response7);
+        //Insert too few value
+        String response12 = sendCommandToServer("INSERT INTO marks VALUES ('Rob', 35);");
+        System.out.println("Insert too few value: " + response12);
+
+        String response8 = sendCommandToServer("INSERT INTO marks VALUES ('Rob', 35, FALSE);");
+        System.out.println(response8);
+        String response9 = sendCommandToServer("INSERT INTO marks VALUES ();");
+        System.out.println(response9);
+        //Invalid attribute name, using reserved words
+        String response10 = sendCommandToServer("INSERT INTO marks VALUES ('AND', 35, TRUE);");
+        System.out.println(response10);
+    }
+
+    @Test
+    public void testDELETE() {
+        String randomName = generateRandomName();
+        sendCommandToServer("CREATE DATABASE " + randomName + ";");
+        sendCommandToServer("USE " + randomName + ";");
+        sendCommandToServer("CREATE TABLE marks (name, mark, pass);");
+        sendCommandToServer("INSERT INTO marks VALUES ('Simon', 65, TRUE);");
+        sendCommandToServer("INSERT INTO marks VALUES ('Sion', 55, TRUE);");
+        sendCommandToServer("INSERT INTO marks VALUES ('Rob', 35, FALSE);");
+        sendCommandToServer("INSERT INTO marks VALUES ('Chris', 20, FALSE);");
+        //Wrong table name
+        String response = sendCommandToServer("delete from test where name == simon;");
+        System.out.println(response);
+        //case-insensitive
+        String response1 = sendCommandToServer("delete from marks where name=='Simon';");
+        System.out.println(response1);
+        //missing where clause
+        String response2 = sendCommandToServer("delete from marks name == Rob;");
+        System.out.println(response2);
+        String response3 = sendCommandToServer("delete from marks where;");
+        System.out.println(response3);
+    }
 }
