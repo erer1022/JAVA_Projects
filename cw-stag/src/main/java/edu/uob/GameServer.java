54d601b87910ae4f5a5f5e4b97671ae59025c1ea
erer1022 <ishtar.f1022@gmail.com>
2024-05-06 00:15:17 +0100

    clean up

--- cw-stag/src/main/java/edu/uob/GameServer.java
+++ cw-stag/src/main/java/edu/uob/GameServer.java
@@ -29,8 +29,8 @@ public final class GameServer {
 
 
     public static void main(String[] args) throws IOException {
-        File entitiesFile = Paths.get("config" + File.separator + "basic-entities.dot").toAbsolutePath().toFile();
-        File actionsFile = Paths.get("config" + File.separator + "basic-actions.xml").toAbsolutePath().toFile();
+        File entitiesFile = Paths.get("config" + File.separator + "extended-entities.dot").toAbsolutePath().toFile();
+        File actionsFile = Paths.get("config" + File.separator + "extended-actions.xml").toAbsolutePath().toFile();
         GameServer server = new GameServer(entitiesFile, actionsFile);
         server.blockingListenOn(8888);
     }
@@ -474,7 +474,6 @@ public final class GameServer {
                 }
             }
         }
-
         // Check if all remaining required subjects are available
         return requiredSubjects.stream().allMatch(allEntities::contains);
     }
